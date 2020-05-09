a03766d19ee104abc6df640647541f5dde768349
Crisca-cyver <63578755+Crisca-cyver@users.noreply.github.com>
2020-05-09 19:14:46 -0300

    checkpoint commit

--- checkpoint.js
+++ checkpoint.js
@@ -28,9 +28,17 @@ function stringMasLarga(strings) {
   // Ej:
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
-
-
-}
+  var largo = 0;
+  var maslargo = 0;
+  
+  for(var i=0; i < strings.length; i++){
+      if(largo < strings[i].length){
+        largo = strings[i].length;
+          maslargo = strings[i];
+      } 
+      return maslargo;   
+   } 
+  }
 
 function buscarAmigo(amigos, nombre) {
   // Escribe una funcion que reciba un array de amigos
@@ -41,13 +49,12 @@ function buscarAmigo(amigos, nombre) {
   // Ej:
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') // retorna { nombre: 'toni', edad: 33 };
-  for (var i = 0; i < amigos.length; i++) {
-    if (amigos[i].nombre === nombre) {
-      return amigos[i];
-    }
-  }
+    for (var i = 0; i < amigos.length; i++){
+      if (amigos[i].nombre === nombre){
+        return amigos[i];
+      }
 }
-
+}
 function sumArray(array, n) {
   // Escribir un algoritmo que, dada un arreglo de números ordenados
   // y un número n, 
@@ -58,8 +65,14 @@ function sumArray(array, n) {
   // sumArray([2,5,7,10,11,15,20], 13)  // retorna true     2+11 suman 13
   // sumArray([2,5,7,10,11,15,20], 14)  // retorna false
   // pista: podes usar bucles anidados;
-  
-};
+  for (index = 0;index < array.length ;index++) {
+    for (aux = index+1; aux < array.length; aux++){
+      if ((array[index]+array [aux]) === n){
+        return true;
+      }
+     } return false;
+}
+}
 
 function pluck(array, propiedad) { 
   // Escribi una función llamada pluck,
@@ -69,27 +82,27 @@ function pluck(array, propiedad) {
   // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
   // productos.pluck(productos, 'name') // ['TV LCD', 'Computadora']
   // pista: es una buena oportunidad para usar map.
-  
+  var salida = [];
+    for (var i=0; i < array.length ; ++i)
+        salida.push(array[i][propiedad]);
+    return salida;  
   
 }
 
 // =======================================================================
-
-
 function crearClasePersona() {
   // Crear una clase para construir objeto de tipo Persona.
   // el constructor debe recibir:
   // nombre (string) , edad (integer) , hobbies (array de strings) , amigos (array de objetos)
   // Esta funcion debe retonar la clase Persona.
 
-  // class Persona {
-  //   constructor()
-  //   {
-  //     this.nombre
-  //     this.edad
-  //     this.hobbies
-  //     this.amigos
-  //     ) {
+ class Persona {
+  constructor(nombre, edad, hobbies, amigos) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.hobbies = hobbies;
+    this.amigos = amigos;
+  
       
     }
 
@@ -99,28 +112,35 @@ function crearClasePersona() {
       // { nombre: nombre, edad: edad} al arreglo
       // de amigos de la persona.
       // no debe retornar nada.
-      
+      var objeto = {
+        nombre: nombre,
+        edad: edad,
+      };
+      Persona.amigos.push();
     }
 
     addHobby(hobby) {
-      // este método debe agregar un hobby (hobby) al arreglo de hobbies de la persona.
+      // este método debe agregar un hobby (hobby) al
+      // arreglo de hobbies de la persona.
       // no debe retornar nada.
-    // hobby = hobbies.push("leer");
+    //hobby = hobbies.push("leer");
   
     }
     getFriends() {
-      // Escribe una función que retorne un arreglo con sólo los nombres del arreglo de amigos
+      // Escribe una función que retorne 
+      //un arreglo con sólo los nombres del arreglo de amigos
       // de la persona.
       // Ej:
       // persona.getFriends() // retorna ['toni', 'Leo', 'Manu']
       // Persona.getFriends();
+     
     }
 
     getHobbies() {
       // Escribe una función que retorne un arreglo con los hobbies de la persona
       // Ej:
       // persona.getHobbies() // retorna ['correr', 'dormir', 'nadar']
-      
+    
     }
 
     getPromedioEdad() {
@@ -137,11 +157,11 @@ function crearClasePersona() {
       // }
       // persona.getPromedioEdad() // retorna 29
     }
-  };
+
+   };
 
   return Persona;
 }
-
 
 /* ====================== EXTRA CREDIT ===================== */
 // Este ejercicio no cuenta en el puntaje del checkpoint
@@ -169,6 +189,7 @@ function filtrar(funcion) {
   // }) => [{price: 100, name:'tv'}]
 
 };
+
 
 // No modifiques nada debajo de esta linea
 // 
